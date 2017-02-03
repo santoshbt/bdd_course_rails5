5ced26634ef78510ba82517ad92959da672d3285
santoshbt <bt.santosh01@gmail.com>
2017-02-03 20:06:33 +0530

    Handle error not found error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,8 +21,17 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  protected
+
+  def resource_not_found
+    message = "The article you are looking out for could not be found."
+    flash[:alert] = message
+    redirect_to root_path
+  end
   
   private
+
   def article_params
     params.require(:article).permit(:title, :body)
   end
